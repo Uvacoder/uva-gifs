b51d3b14a3b4d3ceceec440f13cbeb8776caa8fb
Aaron Bushnell <aaron.bushnell@gmail.com>
2020-11-11 11:02:43 -0500

    Push .jpgs

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/*.gif");
+  eleventyConfig.addPassthroughCopy("src/*.jpg");
+  eleventyConfig.addPassthroughCopy("src/*.jpeg");
   eleventyConfig.addPassthroughCopy("src/.htaccess");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
 };
